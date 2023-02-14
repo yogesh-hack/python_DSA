c5d3fd698ae6ae8c69c7257b698277e1e0043d90
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2023-02-14 11:29:40 +0530

    Create permutation.py

---
+++ permutation.py
@@ -0,0 +1,19 @@
+>>> from itertools import permutations
+>>> print permutations(['1','2','3'])
+<itertools.permutations object at 0x02A45210>
+>>> 
+>>> print list(permutations(['1','2','3']))
+[('1', '2', '3'), ('1', '3', '2'), ('2', '1', '3'), ('2', '3', '1'), ('3', '1', '2'), ('3', '2', '1')]
+>>> 
+>>> print list(permutations(['1','2','3'],2))
+[('1', '2'), ('1', '3'), ('2', '1'), ('2', '3'), ('3', '1'), ('3', '2')]
+>>>
+>>> print list(permutations('abc',3))
+[('a', 'b', 'c'), ('a', 'c', 'b'), ('b', 'a', 'c'), ('b', 'c', 'a'), ('c', 'a', 'b'), ('c', 'b', 'a')]
+
+
+
+from itertools import permutations
+
+s,n = input().split()
+print(*[''.join(i) for i in permutations(sorted(s),int(n))],sep='\n')
