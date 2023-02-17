4c62c45671f8a9e589ac70af6e84a1eb79232ad1
YOGESH BAGHEL <83384315+yogesh-hack@users.noreply.github.com>
2023-02-17 10:24:58 +0530

    Delete python_DSA-1 directory

--- python_DSA-1/python_DSA/python-Data-Science/Multiples.py
+++
@@ -1,9 +0,0 @@
-
-# You are given a task to find all of the whole numbers below 100 that are multiples of both 3 and 5.
-# Create an array of numbers below 100 that are multiples of both 3 and 5, and output it.
-
-import numpy as np
-
-data = np.arange(1,100)
-
-print(data[(data % 3 == 0)  & (data % 5 == 0 )])
